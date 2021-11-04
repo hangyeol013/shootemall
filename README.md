89d56af7f170061778d3f4f2ad2df50e1993afe5
Alexandre Kirchmeyer <alexandre.kirchmeyer@polytechnique.edu>
2021-11-04 01:04:27 +0100

    Functioning video recording + upload to server (computer + phone)

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 Future Steps:
 1) App
 - basic server
-  - // https://www.digitalocean.com/community/tutorials/how-to-create-a-web-server-in-node-js-with-the-http-module
+  - READ THIS: https://binyamin.medium.com/creating-a-node-express-webpack-app-with-dev-and-prod-builds-a4962ce51334
+- libraries
+  - NodeJS: https://nodejs.org/en/about/
+  - express: https://expressjs.com/en/starter/hello-world.html
+  - webpack: https://webpack.js.org/ 
+  - socket.io: https://socket.io/get-started/chat 
 - Ask phone to take video of surroundings
-  - https://www.digitalocean.com/community/tutorials/front-and-rear-camera-access-with-javascripts-getusermedia
-  - https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Signaling_and_video_calling
-  - https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Taking_still_photos#get_the_video
-  - https://webrtc.github.io/samples/src/content/getusermedia/record/
-  - see in particular: https://github.com/webrtc/samples/blob/gh-pages/src/content/getusermedia/record/js/main.js
-- Upload to server
+  - SEE THIS: https://github.com/webrtc/samples/blob/gh-pages/src/content/getusermedia/record/js/main.js
 - Render phone camera
 - Basic UI
 
---
+++ src/server/server.js
@@ -0,0 +1,89 @@
+import path from 'path'
+import express from 'express'
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+import config from '../../webpack.dev.config.js'
+import https from 'https'
+import fs from 'fs'
+import { Server } from "socket.io"
+import compression from 'compression' 
+
+// Express
+// *******
+const app = express()
+const DIST_DIR = __dirname
+const HTML_FILE = path.join(DIST_DIR, 'index.html')
+const compiler = webpack(config)
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}))
+app.use(webpackHotMiddleware(compiler))
+
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    return false
+  }
+  return compression.filter(req, res)
+}
+
+app.use(compression({ filter: shouldCompress }))
+
+app.get('*', (req, res, next) => {
+  compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
+  if (err) {
+    return next(err)
+  }
+  res.set('content-type', 'text/html')
+  res.send(result)
+  res.end()
+  })
+})
+
+// HTTPS
+// *****
+
+const privateKey  = fs.readFileSync('./certs/server.key', 'utf8');
+const certificate = fs.readFileSync('./certs/server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+
+const httpsServer = https.createServer(credentials, app);
+
+// Socket.io
+// *********
+
+const io = new Server(httpsServer);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  var chunks = [];
+  socket.on('chunk', (chunk) => {
+    chunks.push(Buffer.from(chunk));
+    console.log(`Chunk size=${chunk.byteLength}`)
+  });
+  socket.on('upload', () => {
+    console.log(`Received n=${chunks.length} chunks`);
+    // https://stackoverflow.com/questions/40137880/save-video-blob-to-filesystem-electron-node-js
+    const buffer = Buffer.concat(chunks);
+    console.log(`${buffer.length}`)
+    fs.writeFile('video.webm', buffer, {}, (err, res) => {
+        if(err){
+            console.error(err)
+            return
+        }
+        console.log('video saved')
+    })
+    chunks = []
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+// Run
+
+httpsServer.listen(8443, () => {
+  console.log(`App listening to 8443....`)
+  console.log('Press Ctrl+C to quit.')
+})
